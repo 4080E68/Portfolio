ec19b15a3a976b891f46552ae215fd2aceedb2cb
Toad <55682637+4080E68@users.noreply.github.com>
2022-06-09 18:17:08 +0800

    0609

--- resume/app.js
+++ resume/app.js
@@ -11,3 +11,4 @@ const animation = document.querySelector("div.animation");
 lastWord.addEventListener("animationend",() => {
     animation.style = "transition: all 1s ease; opacity: 0; pointer-events:none;";
 })
+
