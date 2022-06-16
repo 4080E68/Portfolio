fc4b0ac5a049354dcd07c7ec59e8e21f120f7a45
Toad <55682637+4080E68@users.noreply.github.com>
2022-06-16 15:31:53 +0800

    0616

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -11,12 +11,17 @@ add.addEventListener("click", (e) => {
   let todoDate = form.children[2].value;
   console.log(todoText, todoMonth, todoDate);
 
+  if (form.children[0].value === "") {
+    alert("請輸入代辦事項!");
+    return; //由此結束~
+  }
+
   //create item
   let todo = document.createElement("div");
   todo.classList.add("todo"); //新增css
   let text = document.createElement("p");
   text.classList.add("todo-text");
-  text.innerText = "事件:"+todoText;
+  text.innerText = "事件:" + todoText;
   let time = document.createElement("p");
   time.classList.add("todo-time");
   time.innerText = "日期:" + todoMonth + "/" + todoDate;
@@ -32,4 +37,48 @@ add.addEventListener("click", (e) => {
   trash.innerHTML = '<i class="fa-regular fa-trash-can"></i>'; //垃圾桶圖標
   todo.appendChild(check);
   todo.appendChild(trash);
+
+  todo.style.animation = "scaleUp 0.5s forwards";
+
+  check.addEventListener("click", (e) => {
+    //確認按鈕觸發事件
+    console.log(e.target.parentElement);
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done"); //toggle與現在狀態相反,有的化增加反之消除
+
+    //check.style.animation = "toUp 0.5s forwards";
+  });
+  trash.addEventListener("click", (e) => {
+    //垃圾桶觸發事件
+    let todoItem = e.target.parentElement;
+    console.log(todoItem);
+    todoItem.style.animation = "scaleLow 0.5s forwards";
+    todoItem.addEventListener("animationend", (e) => {
+      //在動畫結束後執行
+      todoItem.remove(); //移除todoItem
+    });
+
+    //todoItem.remove();
+  });
+
+  let mytodo = {
+    // 將資訊存入object裡面
+    todoText: todoText,
+    todoDate: todoDate,
+    todoMonth: todoMonth,
+  };
+
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    //如果整個localstorage是空的,就設置一個 key為list value為mytodo
+    localStorage.setItem("list", JSON.stringify([mytodo])); //
+  } else {
+    //如果list裡面已經有存放東西的話
+    let myListArray = JSON.parse(myList); //先取得裡面原本的List並使用JSON解析放入myListArray中
+    myListArray.push(mytodo); //將新的資訊mytodo放入myListArray
+    localStorage.setItem("list", JSON.stringify(myListArray)); //將myListArray使用JSON再次存入至localStorage。
+  }
+  
+
+  form.children[0].value = ""; //清空輸入欄位
 });
